5ec03e155ee728f2baff2f05889c97862d4d7835
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-01-25 22:02:45 -0600

    Add files via upload

--- src/assignments/main_assignment2.py
+++ src/assignments/main_assignment2.py
@@ -1,4 +1,8 @@
 from assignment2 import faculty_evaluation_result
 
+faculty_evaluation_result()
 '''Write code to call the faculty_evaluation_result function with data of your choice'''
+
 print('Hello')
+
+

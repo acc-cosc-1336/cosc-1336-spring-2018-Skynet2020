92a881befd145771c71ebb822bbced1cb395345f
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-01-25 22:30:33 -0600

    Add files via upload

--- src/assignments/assignment2.py
+++ src/assignments/assignment2.py
@@ -1,6 +1,3 @@
-
-
-
 def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
 ''' Write code to calculate faculty evaluation rating according to asssignment instructions
 
@@ -10,26 +7,26 @@ def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
     :param oft: Often
     :param voft: Very Often
     :param alw: Always
-    :return: rating as a string
-    '''
+    :return: rating as a string'''
+
     total = nev + rar + som + oft + voft + alw
     nev_ratio = nev / total
-    rar_ratio =  rar / total
+    rar_ratio = rar / total
     som_ratio = som / total
     oft_ratio = oft / total
     voft_ratio = voft / total
     alw_ratio = alw / total
 
     if alw_ratio + voft_ratio >= 0.9:
-        return str("Excellent")
+        return "Excellent"
     elif alw_ratio + voft_ratio + oft_ratio >= 0.8:
-        return str("Very Good")
+        return "Very Good"
     elif alw_ratio + voft_ratio + oft_ratio + som_ratio >= 0.7:
-        return str("Good")
+        return "Good"
     elif alw_ratio + voft_ratio + oft_ratio + som_ratio + rar_ratio >= 0.6:
-        return str("Needs Improvement")
+        return "Needs Improvement"
     else:
-        return str("Unacceptable")
+        return "Unacceptable"
         
   
 

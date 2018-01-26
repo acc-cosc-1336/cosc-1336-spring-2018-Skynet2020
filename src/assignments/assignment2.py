f400c293e8a9fa68e92ee84a583ba369f9105701
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-01-25 21:50:37 -0600

    Add files via upload

---
+++ src/assignments/assignment2.py
@@ -0,0 +1,73 @@
+
+
+
+def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
+    total = nev + rar + som + oft + voft + alw
+    nev_ratio = nev / total
+    rar_ratio =  rar / total
+    som_ratio = som / total
+    oft_ratio = oft / total
+    voft_ratio = voft / total
+    alw_ratio = alw / total
+
+    if alw_ratio + voft_ratio >= 0.9:
+##        return "Excellent"
+        result = "Excellent"
+        print(result)
+    elif alw_ratio + voft_ratio + oft_ratio >= 0.8:
+##        return "Very Good"
+        result = "Very Good"
+        print(result)
+    elif alw_ratio + voft_ratio + oft_ratio + som_ratio >= 0.7:
+##        return "Good"
+        result = "Good"
+        print(result)
+    elif alw_ratio + voft_ratio + oft_ratio + som_ratio + rar_ratio >= 0.6:
+##        return "Needs Improvement"
+        result = "Needs Improvement"
+        print(result)
+    else:
+##        return "Unacceptable"
+        result = "Unacceptable"
+        print(result)
+
+faculty_evaluation_result(2, 3, 20, 25, 8, 7)
+
+##    elif alw_ratio + voft_ratio + oft_ratio + som_ratio + rar_ratio + nev_ratio < 0.6:
+##        return "Unacceptable"
+    
+
+
+    
+
+
+
+''' Write code to calculate faculty evaluation rating according to asssignment instructions
+
+    :param nev: Never
+    :param rar: Rarely
+    :param som: Sometimes
+    :param oft: Often
+    :param voft: Very Often
+    :param alw: Always
+    :return: rating as a string
+    '''
+
+def get_ratings(nev,rar,som, oft,voft, alw):
+    '''
+    Students aren't expected to know this material yet!
+    '''
+    ratings = []
+    total = nev + rar + som + oft + voft + alw
+
+    ratings.append(round(alw / total, 2))
+    ratings.append(round(voft / total, 2))
+    ratings.append(round(oft / total, 2))
+    ratings.append(round(som / total, 2))
+    ratings.append(round(rar / total, 2))
+    ratings.append(round(nev / total, 2))
+
+    return ratings
+    
+
+    

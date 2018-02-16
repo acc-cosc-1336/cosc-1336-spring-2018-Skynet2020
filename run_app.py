bdacab52ba858146d6573c67422434afbcc7ff3e
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-02-15 21:52:19 -0600

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,4 +1,3 @@
-import src.homework.main.main_homework4
-
+import src.assignment.main_assignment.py
 #running this file will run the code in main_homework2.py file without the need to modify
 #code in that file

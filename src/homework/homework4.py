38a27f5c6df2cd1f0e4476d23b000f378976e9a8
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-02-19 23:37:29 -0600

    Homework 4 help
    Valid_letter_grade function should return True if letter_grade in range false otherwise.
    Get_credit_points should return an integer, the homework didn't ask for validation.  However if you wanted to validate, you would do it in main_homework4 file with a while loop.
    Remove extra code or blank lines.

--- src/homework/homework4.py
+++ src/homework/homework4.py
@@ -1,28 +1,14 @@
 def sample_function(value):
     '''Return value given'''
     return value
-##-------------------------------------------------------
-
-
 
 def valid_letter_grade(letter_grade):
     x = letter_grade        
-    if x == 'A' or x == 'a':
-        letter_grade = x
-    elif x == 'B' or x == 'b':
-        letter_grade = x
-    elif x == 'C' or x == 'c':
-        letter_grade = x
-    elif x == 'D' or x == 'd':
-        letter_grade = x
-    elif x == 'F' or x == 'f':
-        letter_grade = x
-##    else:
-##        print('Incorrect grade')
-##        x = letter_grade
-    return letter_grade
-##------------------------------------------------------
-
+    if x == 'A' or x == 'a' or x == 'B' or x == 'b' or x == 'C' or x == 'c' or \
+       x == 'D' or x == 'd' or x == 'F' or x == 'f':
+        return True
+    else:
+        return False
 
 def get_credit_points(letter_grade):
     '''
@@ -32,8 +18,8 @@ def get_credit_points(letter_grade):
     :return: a whole number representing the credit points
     WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
     '''
-    y = valid_letter_grade(letter_grade)
     credit_points = 0
+    y = letter_grade
     if y == 'A' or y == 'a':
         credit_points = 4        
     elif y == 'B' or y == 'b':
@@ -42,16 +28,10 @@ def get_credit_points(letter_grade):
         credit_points = 2        
     elif y == 'D' or y == 'd':
         credit_points = 1        
-    elif y == 'F' or y == 'f':
-        credit_points = 0        
     else:
-        print('Seems that you entered an incorect letter grade')
-        x = input('Enter a valid letter grade here: ')
-        print(credit_points)
+        credit_points = 0        
         
     return credit_points
-##-------------------------------------------------------
-
 
 def get_grade_points(credit_hours, credit_points):
     '''
@@ -62,14 +42,7 @@ def get_grade_points(credit_hours, credit_points):
     :return: Total grade points for a class
     '''
     grade_points = credit_hours * credit_points
-    print('Grade point for this class is: ', format(grade_points, '.2f'))
     return grade_points
-
-
-##--------------------------------------------------------
-
-
-##grade_points = get_grade_points(credit_hours, credit_points)
 
 def get_grade_point_average(credit_hours, grade_points):
     '''
@@ -80,7 +53,4 @@ def get_grade_point_average(credit_hours, grade_points):
     WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
     '''
     GPA = grade_points / credit_hours
-    print('GPA is: ', format(GPA, '.2f'))
     return GPA
-
-##get_grade_point_average(credit_hours, grade_points)

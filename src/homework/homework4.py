8eac9e0ae48e342d7e7f0061d9c145861991b1fc
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-02-20 00:00:00 -0600

    Update homework4.py

--- src/homework/homework4.py
+++ src/homework/homework4.py
@@ -1,28 +1,15 @@
 def sample_function(value):
     '''Return value given'''
     return value
-##-------------------------------------------------------
-
-
 
 def valid_letter_grade(letter_grade):
     x = letter_grade        
-    if x == 'A' or x == 'a':
-        letter_grade = x
-    elif x == 'B' or x == 'b':
-        letter_grade = x
-    elif x == 'C' or x == 'c':
-        letter_grade = x
-    elif x == 'D' or x == 'd':
-        letter_grade = x
-    elif x == 'F' or x == 'f':
-        letter_grade = x
-##    else:
-##        print('Incorrect grade')
-##        x = letter_grade
+    if x == 'A' or x == 'a' or x == 'B' or x == 'b' or x == 'C' or x == 'c' or \
+ +       x == 'D' or x == 'd' or x == 'F' or x == 'f':
+ +        return True
+ +  else:
+ +        return False
     return letter_grade
-##------------------------------------------------------
-
 
 def get_credit_points(letter_grade):
     '''
@@ -32,8 +19,9 @@ def get_credit_points(letter_grade):
     :return: a whole number representing the credit points
     WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
     '''
-    y = valid_letter_grade(letter_grade)
+    
     credit_points = 0
+    y = letter_grade
     if y == 'A' or y == 'a':
         credit_points = 4        
     elif y == 'B' or y == 'b':
@@ -41,17 +29,11 @@ def get_credit_points(letter_grade):
     elif y == 'C' or y == 'c':
         credit_points = 2        
     elif y == 'D' or y == 'd':
-        credit_points = 1        
-    elif y == 'F' or y == 'f':
-        credit_points = 0        
+        credit_points = 1
     else:
-        print('Seems that you entered an incorect letter grade')
-        x = input('Enter a valid letter grade here: ')
-        print(credit_points)
+        credit_points = 0
         
     return credit_points
-##-------------------------------------------------------
-
 
 def get_grade_points(credit_hours, credit_points):
     '''
@@ -62,14 +44,7 @@ def get_grade_points(credit_hours, credit_points):
     :return: Total grade points for a class
     '''
     grade_points = credit_hours * credit_points
-    print('Grade point for this class is: ', format(grade_points, '.2f'))
     return grade_points
-
-
-##--------------------------------------------------------
-
-
-##grade_points = get_grade_points(credit_hours, credit_points)
 
 def get_grade_point_average(credit_hours, grade_points):
     '''
@@ -80,7 +55,4 @@ def get_grade_point_average(credit_hours, grade_points):
     WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
     '''
     GPA = grade_points / credit_hours
-    print('GPA is: ', format(GPA, '.2f'))
     return GPA
-
-##get_grade_point_average(credit_hours, grade_points)

9784c0dc1ca2dff3a7ebd19ecf0a73ce3d50d786
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-02-19 21:42:01 -0600

    Add files via upload

--- tests/homework/test_homework4.py
+++ tests/homework/test_homework4.py
@@ -29,33 +29,35 @@ from src.homework.homework4 import valid_letter_grade
 
 class TestHomework4(unittest.TestCase):
 
+    class TestHomework4(unittest.TestCase):
+
     def valid_letter_grade_w_value_B(self):
-        self.assertEqual('B', valid_letter_grade(B));
+        self.assertEqual('B', valid_letter_grade('B'));
     def valid_letter_grade_w_value_Z(self):
-        self.assertEqual('Seems that you entered an incorect letter grade', valid_letter_grade(B));
+        self.assertEqual('Seems that you entered an incorect letter grade', valid_letter_grade('Z'));
     def get_credit_points_w_value_A(self):
-        self.assertEqual(4, valid_letter_grade(A));
+        self.assertEqual(4, get_credit_points('A'));
     def get_credit_points_w_value_B(self):
-        self.assertEqual(3, valid_letter_grade(B));
+        self.assertEqual(3, get_credit_points('B'));
     def get_credit_points_w_value_C(self):
-        self.assertEqual(2, valid_letter_grade(C));
+        self.assertEqual(2, get_credit_points('C'));
     def get_credit_points_w_value_D(self):
-        self.assertEqual(1, valid_letter_grade(D));
+        self.assertEqual(1, get_credit_points('D'));
     def get_credit_points_w_value_F(self):
-        self.assertEqual(0, valid_letter_grade(F));
+        self.assertEqual(0, get_credit_points('F'));
     def get_credit_points_w_value_a(self):
-        self.assertEqual(4, valid_letter_grade(a));
+        self.assertEqual(4, get_credit_points('a'));
     def get_credit_points_w_value_b(self):
-        self.assertEqual(3, valid_letter_grade(b));
+        self.assertEqual(3, get_credit_points('b'));
     def get_credit_points_w_value_c(self):
-        self.assertEqual(2, valid_letter_grade(c));
+        self.assertEqual(2, get_credit_points('c'));
     def get_credit_points_w_value_d(self):
-        self.assertEqual(1, valid_letter_grade(d));
+        self.assertEqual(1, get_credit_points('d'));
     def get_credit_points_w_value_f(self):
-        self.assertEqual(0, valid_letter_grade(f));
+        self.assertEqual(0, get_credit_points('f'));
     def get_grade_points_w_value_3_and_4(self):
-        self.assertEqual(12, valid_letter_grade(3, 4));
+        self.assertEqual(12, get_grade_points(3, 4));
     def get_grade_point_average_w_value_9_and_36(self):
-        self.assertEqual(4.00, valid_letter_grade(9.00, 36.00));
+        self.assertEqual(4.00, get_grade_point_average(9.00, 36.00));
 
 unittest.main(verbosity = 2)

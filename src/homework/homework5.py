a30e4fdf2ac54996a7518e107c7326559606540e
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-02-25 17:02:41 -0600

    Homework 5 help
    Write sales data function missing file_object parameter.
    Read sales data function must use a loop to read unknown number of records.

--- src/homework/homework5.py
+++ src/homework/homework5.py
@@ -4,57 +4,11 @@
 #Create another function named read_sales_data with file_object as a parameter.
 #The function will read the file line by line and display to screen to produce the table described in homework 5.
 
-def write_sales_data(item, price):
-    outfile = open('file_object.txt', 'w')
+def write_sales_data(file_object, item, price):
 
-    price_1 = float(input('Enter the price for the item 1: '))
-    price_2 = float(input('Enter the price for the item 2: '))
-    price_3 = float(input('Enter the price for the item 3: '))
-    
-    total_price = price_1 + price_2 + price_3
-    average_price = (price_1 + price_2 + price_3)/3
-    outfile.write('Item'+\
-                  '\t'+\
-                  'Price\n')
-    outfile.write('Hamburger' +\
-                  '\t'+\
-                  str(price_1)+\
-                  '\n')
-    outfile.write('Fries'+\
-                  '\t'+\
-                  str(price_2)+\
-                  '\n')
-    outfile.write('Drink'+\
-                  '\t'+\
-                  str(price_3)+\
-                  '\n')
-    outfile.write('Total'+\
-                  '\t'+\
-                  str(total_price)+\
-                  '\n')
-    outfile.write('Avg Price'+\
-                  '\t'+\
-                  format(average_price, '.2f')+\
-                  '\n')
-    
-    outfile.close()
-    print('Data written to the file')
+    file_object.write(item + '\t' + price + '\n')
 
 def read_sales_data(file_object):
-    infile = open('file_object.txt', 'r')
 
-    action_1 = str(infile.readline())
-    action_2 = str(infile.readline())
-    action_3 = str(infile.readline())
-    action_4 = str(infile.readline())
-    action_5 = str(infile.readline())
-    action_6 = str(infile.readline())
-
-    infile.close
-
-    print(action_1)
-    print(action_2)
-    print(action_3)
-    print(action_4)
-    print(action_5)
-    print(action_6)
+    for line in file_object:
+        print(line.rstrip('\n'))

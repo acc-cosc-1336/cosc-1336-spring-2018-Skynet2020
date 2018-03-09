3a4466a58ff549d8c2963e29f88692861405d0d3
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-03-08 22:05:05 -0600

    Update exam_test.py

--- tests/midterm/exam_test.py
+++ tests/midterm/exam_test.py
@@ -1,1 +1,57 @@
+import unittest
+from exam import get_miles_per_hour, get_bonus_pay_amount
+
+#write import statements for exam functions
+
+class Test_Midterm(unittest.TestCase):
+
+    def test_get_miles_per_hour(self):
+        '''
+        5 points
+        Test with arguments kilometers 32 and minutes 60 return value should be 19.883872
+        '''
+        self.assertEqual(19.88, get_miles_per_hour(32, 60))
+
+
+    def test_get_bonus_pay_amount_w_good_value (self):
+        '''
+        5 points
+        Test with value 1000 return value should be 70
+        '''
+        self.assertEqual(7, get_bonus_pay_amount(1000))
+
+
+    def test_get_bonus_pay_amount_w_bad_value(self):
+        '''
+        5 points
+        Test with -5 return value should be 'Invalid arguments'
+        '''
+        self.assertEqual('Invalid argument', get_bonus_pay_amount(-5))
+
+
+
+    def test_reverse_string(self):
+        '''
+        5 points
+        Test with value My String Data return value should be ataD gnirtS yM
+        '''
+
+
+
+    def test_get_list_min_max(self):
+        '''
+        5 points
+        Test with ['joe', 10, 15, 20, 30, 40]    Returns:    [10, 40]
+        '''
+
+
+
+    def test_get_list_min_max_file(self):
+        '''
+        5 points
+        Test with quiz.data file the return value should be [2,89]
+        '''
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 

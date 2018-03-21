e24aedb1b54fa2f02e9a3c2f646f93bd6e461e1c
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-03-21 15:04:43 -0500

    Update test_homework7.py

--- tests/homework/test_homework7.py
+++ tests/homework/test_homework7.py
@@ -2,7 +2,19 @@ import unittest
 from src.homework.homework7 import get_p_distance_matrix, print_get_p_distance_matrix
 #write import statement for homework 7 file
 
-
+arr = [
+      ['T','T','T','C','C','A','T','T','T','A'],
+      ['G','A','T','T','C','A','T','T','T','C'],
+      ['T','T','T','C','C','A','T','T','T','T'],
+      ['G','T','T','C','C','A','T','T','T','A']
+      ]
+    
+result = [
+         [0.0, 0.4, 0.1, 0.1],
+         [0.4, 0.0, 0.4, 0.3],
+         [0.1, 0.4, 0.0, 0.2],
+         [0.1, 0.3, 0.2, 0.0]
+         ]
 class TestHomework7(unittest.TestCase):
 
     def test_sample(self):
@@ -25,15 +37,9 @@ class TestHomework7(unittest.TestCase):
     0.10000 0.30000 0.20000 0.00000
 
     '''
-    arr = [
-          ['T','T','T','C','C','A','T','T','T','A'],
-          ['G','A','T','T','C','A','T','T','T','C'],
-          ['T','T','T','C','C','A','T','T','T','T'],
-          ['G','T','T','C','C','A','T','T','T','A']
-          ]
-
+    
     def test_get_p_distance_matrix(self):
-        self.assertEqual(print_get_p_distance_matrix(arr), arr)
+        self.assertEqual(result, get_p_distance_matrix(arr))
 
     
 if __name__ == '__main__':

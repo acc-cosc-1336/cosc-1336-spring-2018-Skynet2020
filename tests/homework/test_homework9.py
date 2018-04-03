da822e25ba11bbd38d65266689aa140aad169ea6
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-04-03 17:06:14 -0500

    Add files via upload

---
+++ tests/homework/test_homework9.py
@@ -0,0 +1,20 @@
+import unittest
+from die import Die
+#Write the import statement for the Die class
+
+class TestHomework9(unittest.TestCase):
+
+    sideup = Die()
+    
+    def test_rolls_values_1_to_6(self):
+        '''
+        Write a test case to ensure that the Die class only rolls values from 1 to 6
+
+        '''
+        sideup = Die()
+        self.assertTrue(0 < sideup.roll() < 7)
+
+
+
+if __name__ == '__main__':
+  unittest.main(verbosity = 2)

05ca0865dbbd7200a50f96518e80c317728dcff5
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-04-05 22:10:32 -0500

    Add files via upload

---
+++ src/assignments/assignment10/invoice_item.py
@@ -0,0 +1,27 @@
+from product import Product
+##src.assignments.assignment10.
+
+class InvoiceItem:
+           
+    def __init__(self, quantity, product):
+        '''
+        ASSIGNMENT10: 
+        Remove description and cost parameters. Replace with a product class
+        '''
+
+        #ASSIGNMENT10: update code to use product class
+        self.product = product
+        self.quantity = quantity
+        
+
+    def get_extended_cost(self):
+        '''
+        Write a statement to multiply quantity * cost and return the result
+        :return:  extended cost
+        '''
+        #ASSIGNMENT10: MODIFY CODE TO GET THE COST FROM THE PRODUCT ATTRIBUTE
+        return self.product.cost * self.quantity
+
+    def get_description(self):
+        #ASSIGNMENT10: MODIFY CODE TO GET THE NAME FROM THE PRODUCT ATTRIBUTE
+        return self.product.name

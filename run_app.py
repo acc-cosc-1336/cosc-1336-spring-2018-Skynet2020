5e8960ab4d77011881688a39cc0529c6fe9758ed
Skynet2020 <35587043+Skynet2020@users.noreply.github.com>
2018-04-12 21:37:58 -0500

    Add files via upload

--- run_app.py
+++ run_app.py
@@ -1,4 +1,7 @@
-from src.homework.main import main_homework
+
 
 #running this file will run the code in main_homework2.py file without the need to modify
 #code in that file
+import main
+
+#src.assignments.assignment10.
